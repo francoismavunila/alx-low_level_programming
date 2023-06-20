8c53790c15e9818c46f881facc4ac893959a2dea
francois <mavunila002@gmail.com>
2023-06-20 12:44:39 -0700

    message

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,16 +6,15 @@
  * Return:0
  */
 
-char characters[8]="_putchar";
-
 int main(void)
 {
-    int i=0;
-    for(i=0;i<=7;i++)
-    {
-        _putchar(characters[i]);
-    }
-    _putchar('\n');
-    return 0;
+	char characters[8]="_putchar";
+    	int i=0;
+    	for(i=0;i<=7;i++)
+    	{
+        	_putchar(characters[i]);
+    	}
+    	_putchar('\n');
+    	return 0;
 }
 
