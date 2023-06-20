c128b5424d598f0b35d20cc9c33f2bbd2b222b59
francois <mavunila002@gmail.com>
2023-06-20 13:14:27 -0700

    message

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return:0
+ */
+int main(void)
+{
+    char characters = 'a';
+    while(characters <= 'z')
+    {
+        _putchar(characters);
+        characters++;
+    }
+    _putchar('\n');
+    return 0;
+}
+
