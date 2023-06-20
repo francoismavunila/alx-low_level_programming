304a314ecd06c916200b8d44a929a4847bc2e03d
francois <mavunila002@gmail.com>
2023-06-20 15:09:07 -0700

    message

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return:0
+ */
+int _abs(int x)
+{
+    if(x>=0)
+    {
+        return x;
+    }
+    else
+    {
+        x=-1*x;
+        return x;
+    }
+}
