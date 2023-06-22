87d874275653a0b3e9ccc8bdcd5ac5bc4537ce43
francois <mavunila002@gmail.com>
2023-06-22 16:46:28 -0700

    message

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for a digit
+ *   check
+ * Return:0 or 1
+ */
+
+int mul(int a, int b)
+{
+        int results ;
+        result = a*b;
+        return result;
+}
