69e1aa00ae2cacaef48732c280f4581ce43a1676
francois <mavunila002@gmail.com>
2023-06-26 16:21:52 -0700

    mesage

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * pointers and arrays
+ * 
+ */
+void puts_half(char *str)
+{
+    int length=0;
+    while(*str != '\0')
+    {
+        length++;
+        str++;
+    }
+    if((length%2)!=0)
+    {
+        length=(length-1)/2;
+    }else
+    {
+        length = length/2;
+    }
+    while (length>=0)
+    {
+         _putchar(*(str-length));
+    }
+    
+}
