7b48b920eb75068d422f2cce92014f64ebb751a7
francois <mavunila002@gmail.com>
2023-06-26 15:32:27 -0700

    mesage

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * pointers and arrays
+ * 
+ */
+void rev_string(char *s)
+{
+    int length=0;
+    char temp;
+    while(*s != '\0')
+    {
+        length++;
+        s++;
+    }
+    while(length>1)
+    {
+        s--;
+        temp=*s;
+        *s = *(s-length);
+        length=length-2;
+    }
+    
+    _putchar('\n');
+}
