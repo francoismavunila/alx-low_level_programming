40a342dcd1a8047aef2a2f9d38bdb571862e3764
francois <mavunila002@gmail.com>
2023-06-26 14:33:49 -0700

    mesage

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * pointers and arrays
+ * 
+ */
+void swap_int(int *a, int *b)
+{
+    int b = *b;
+    *b = *a;
+    *a = b;
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,4 +5,4 @@
 
 int print_vals(char c);
 void reset_to_98(int *n);
-
+void swap_int(int *a, int *b);
