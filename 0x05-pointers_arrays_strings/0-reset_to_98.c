0d37681ed595547294705f8ad206cbeea20e5df6
francois <mavunila002@gmail.com>
2023-06-26 14:23:29 -0700

    mesage

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * pointers and arrays
+ * 
+ */
+void reset_to_98(int *n)
+{
+        *n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#define MAIN_H
+/*
+*main file
+*/
+
+int print_vals(char c);
+void reset_to_98(int *n);
+
