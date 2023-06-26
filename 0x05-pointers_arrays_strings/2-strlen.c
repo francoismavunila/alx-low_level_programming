85d7fc78165a99fc50c200a8f30f50d94b11781a
francois <mavunila002@gmail.com>
2023-06-26 14:49:27 -0700

    mesage

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * pointers and arrays
+ * 
+ */
+int _strlen(char *s)
+{
+    int length=0;
+    while(*s != '\0')
+    {
+        length++;
+        s++;
+    }
+    
+    return length;
+}
