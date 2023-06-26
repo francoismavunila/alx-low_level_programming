59484512ee3e5bb86547ce3066a63d9cd7aaaeb3
francois <mavunila002@gmail.com>
2023-06-26 15:04:10 -0700

    mesage

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * pointers and arrays
+ * 
+ */
+void print_rev(char *s)
+{
+    int length=0;
+    while(*s != '\0')
+    {
+        length++;
+        s++;
+    }
+    
+    while(length>=0)
+    {
+        *s--;
+        _putchar(*s);
+        length--;
+    }
+}
