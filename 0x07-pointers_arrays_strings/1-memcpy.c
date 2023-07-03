9ffce16e514f4c98a7f4f574ca9c19c39f8006f2
francois <mavunila002@gmail.com>
2023-07-03 16:10:24 -0700

    message

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * pointers and arrays
+ * 
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+
