7a698bb30897dc58d296bc5373bc3f238eb1c3df
francois <mavunila002@gmail.com>
2023-07-03 16:04:26 -0700

    message

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,13 +5,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    char *rtn = *s;
-    while(n>0)
-    {
-        *s=b;
-        s++;
-        n--;
-    }
+	unsigned int i;
 
-    return rtn;
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
