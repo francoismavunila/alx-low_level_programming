fd444ca633964222d7f5e21323810ebcd546064f
francois <mavunila002@gmail.com>
2023-07-05 15:56:48 -0700

    message

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,13 +5,10 @@
  */
 void _puts_recursion(char *s)
 {
-       // _putchar(*s);
-       	if (*s == '\0')
-            {
+       	if (*s == '\0'){
                 _putchar('\n');
             }
-        else
-        {
+        else{
             _putchar(*s);
             _puts_recursion(s + 1);
         }
