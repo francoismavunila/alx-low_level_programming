577c57031870e000b6bd959bb4bfcdc0c3d9a559
francois <mavunila002@gmail.com>
2023-07-05 16:41:49 -0700

    message

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * pointers and arrays
+ * 
+ */
+int _pow_recursion(int x, int y)
+{
+    if(y == 1)
+    {
+        return x;
+    }
+    y--;
+    return(x*_pow_recursion(x,y));
+}
