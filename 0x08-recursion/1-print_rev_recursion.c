5b5728345563086a180df4b02e662ddd8c87deda
francois <mavunila002@gmail.com>
2023-07-05 16:11:23 -0700

    message

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,12 +7,12 @@ void _print_rev_recursion(char *s)
 {
         if(*s=='\0')
         {
-                _putchar(*s);
-                *s='\0';
-                
-        }else
+                return;     
+        }
+        else
         {
                 _print_rev_recursion(s+1);
+                _putchar(*s);
         }
     
 }
