baf98f92ad63fa9fef6130b6e3756b36b0ea4825
francois <mavunila002@gmail.com>
2023-07-09 16:36:11 -0700

    message

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * pointers and arrays
+ * 
+ */
+int main(int argc, char *argv[])
+{
+    if (argc>2){
+        int value1 = atoi(argv[1]);
+        int value2 = atoi(argv[2]);
+        int result = value1 * value2;
+        printf("%d\n",result);
+    }else{
+        printf("Error");
+    }
+
+    
+	return (0);
+}
