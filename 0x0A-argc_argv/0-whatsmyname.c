4e6e489e20bdec4a9f7b60de287c9dae903c056f
francois <mavunila002@gmail.com>
2023-07-09 15:41:03 -0700

    message

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * pointers and arrays
+ * 
+ */
+int main(int argc, char *argv[])
+{
+	if (argc>0)
+	{
+    printf("%s",argv[0]);
+	}
+    printf("\n");
+    return 0;
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+a readmore file
