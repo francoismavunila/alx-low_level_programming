b7b880fdae970cc8bab158c4194d98e81bfb1f26
francois <mavunila002@gmail.com>
2023-07-09 16:05:19 -0700

    message

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * pointers and arrays
+ * 
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
+
+		printf("%d\n", i - 1);
+	}
+
+	return (0);
+}
