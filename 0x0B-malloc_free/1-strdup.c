8d420a3cf0ccee12ffb1cecb5d8d5ed15c10ea0b
francois <mavunila002@gmail.com>
2023-07-11 18:48:10 -0700

    message

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,23 +2,28 @@
 #include <unistd.h>
 
 char* _strdup(char* str) {
+    size_t len = 0;
+    char* result;
+    size_t i;
+
     if (str == NULL) {
         return NULL;
     }
-    int len = 0;
+    
     while (str[len] != '\0') {
         len++;
     }
-    char* result = (char*) malloc((len + 1) * sizeof(char));
+    
+    result = (char*) malloc((len + 1) * sizeof(char));
     if (result == NULL) {
         return NULL;
     }
-    int i = 0;
-    while (i < len) {
+    
+    for (i = 0; i < len; i++) {
         write(1, &str[i], 1);
         result[i] = str[i];
-        i++;
     }
+    
     write(1, "\n", 1);
     result[i] = '\0';
     return result;
