846beaf312b6db67e1275d9ec7ad2214fc87d4b6
francois <mavunila002@gmail.com>
2023-07-11 17:47:05 -0700

    message

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
     if (arr == NULL) {
         return NULL;
     }
-    int i;
+    unsigned int i;
     for (i = 0; i < size; i++) {
         arr[i] = c; 
     }
