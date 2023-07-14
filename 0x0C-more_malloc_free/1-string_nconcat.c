439b42cad417c9fe6fd214dbc0f309a3da6aa67b
francois <mavunila002@gmail.com>
2023-07-14 09:01:24 -0700

    message

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,25 +2,29 @@
 #include <string.h>
 
 char *string_nconcat(char *s1, char *s2, unsigned int n) {
+    size_t s1_len = 0;
+    size_t s2_len = 0;
+    char *result;
+    size_t i;
     if (s1 == NULL) {
         s1 = ""; // treat NULL as an empty string
     }
-    size_t s1_len = 0;
+    
     while (s1[s1_len] != '\0') {
         s1_len++;
     }
-    size_t s2_len = 0;
+    
     while (s2[s2_len] != '\0') {
         s2_len++;
     }
     if (n >= s2_len) {
         n = s2_len; // use entire s2 if n >= s2_len
     }
-    char *result = malloc(s1_len + n + 1); // allocate memory for result
+    result = malloc(s1_len + n + 1); // allocate memory for result
     if (result == NULL) {
         exit(EXIT_FAILURE); // exit if malloc fails
     }
-    size_t i;
+    
     for (i = 0; i < s1_len; i++) {
         result[i] = s1[i]; // copy s1 into result
     }
