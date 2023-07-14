16a28dc9034baf11c8a8715c0b2927a0c3ce9f88
francois <mavunila002@gmail.com>
2023-07-14 09:32:52 -0700

    message

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,18 +1,27 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * pointers and arrays
  * 
  */
-void *_calloc(unsigned int nmemb, unsigned int size) {
-    if (nmemb == 0 || size == 0) {
-        return NULL; // return NULL if nmemb or size is 0
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *blk;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	blk = malloc(nmemb * size);
+	if (blk != NULL)
+    {
+		for (i = 0; i < (nmemb * size); i++)
+			blk[i] = 0;
+		return (blk);
+	}
+	else
+    {
+		return (NULL);
     }
-    void *ptr = malloc(nmemb * size); // allocate memory for array
-    if (ptr == NULL) {
-        return NULL; // return NULL if malloc fails
-    }
-    // set memory to zero using memset
-    memset(ptr, 0, nmemb * size);
-    return ptr;
 }
