e5811107db43b22cc9e2c3641f64289b51381d68
francois <mavunila002@gmail.com>
2023-07-14 08:27:29 -0700

    message

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * pointers and arrays
+ * 
+ */
+void *malloc_checked(unsigned int b)
+{
+    char *status = malloc(b);
+    if(status == "NULL")
+    {
+        exit(EXIT_FAILURE);
+    }else{
+        return status;
+    }
+}
