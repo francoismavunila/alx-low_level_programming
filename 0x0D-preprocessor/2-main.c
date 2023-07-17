8614252905fcf1d5da9e56b3c355417f7b41e545
francois <mavunila002@gmail.com>
2023-07-17 15:03:18 -0700

    message

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * commnets
- * on 
- * __FILE__
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
 
 int main(void)
 {
-	print("%s\n", __FILE__);
-       	return(0)	
+	printf("%s\n", __FILE__);
+	return (0);
 }
+
