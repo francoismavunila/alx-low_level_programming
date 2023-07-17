96d4f86b09926ccb8e1b7554d5fa33f51a5108de
francois <mavunila002@gmail.com>
2023-07-17 15:55:58 -0700

    message

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * comments here
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->owner = owner;
+		d->age = age;
+	}
+}
+
+
