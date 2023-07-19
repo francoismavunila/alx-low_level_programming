51a8d2670efd8ad9906b7619a174c49208b3c18e
francois <mavunila002@gmail.com>
2023-07-19 05:52:46 -0700

    message

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,12 +3,16 @@
 /*
 *comments comments about function pointers
 */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-        int count;
-        
-        for(count=0;count<size;count++)
-        {
-           (*action)(array[count]);
-        }
+	unsigned int count;
+
+	if (array && size && action)
+	{
+		for (count = 0; count < size; count++)
+		{
+			(*action)(array[count]);
+		}
+	}
 }
