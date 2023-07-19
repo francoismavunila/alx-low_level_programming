504b1f890e2db137c166f9b5791d40adb0503f75
francois <mavunila002@gmail.com>
2023-07-19 09:19:03 -0700

    message

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,29 +1,26 @@
-#ifndef FILE_C
-#define FILE_C
+#ifndef FILE_F_POINTERS
+#define FILE_F_POINTERS
 
-/**
- * comment comment
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int _putchar(char c);
 
-typedef struct op
-{
-	char *op;
-	int (*f)(int a, int b);
-} op_t;
+void print_name(char *name, void (*f)(char *));
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+int int_index(int *array, int size, int (*cmp)(int));
 
 int op_add(int a, int b);
-
 int op_sub(int a, int b);
-
 int op_mul(int a, int b);
-
 int op_div(int a, int b);
-
 int op_mod(int a, int b);
-
 int (*get_op_func(char *s))(int, int);
 
 #endif
+
 
 
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,7 +1,4 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "3-calc.h"
-#include <string.h>
 
 /**
  *comment comment
