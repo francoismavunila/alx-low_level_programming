3284d674e364e65b1cdee89a5709058760cd0f87
francois <mavunila002@gmail.com>
2023-07-20 17:34:10 -0700

    message

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * sum_them_all - function that returns the sum of all its parameters
+ * @n: number of parameters passed to the function
+ *
+ * Return: sum of all parameters
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+        va_list args;
+        va_start(args,n);
+        int count;
+        int number;
+        for(count=0; count<n; count++)
+        {
+            number = va_arg(args, int);
+            printf("%d", number);
+            if((*separator != NULL) & (count<n-1))
+            {
+                printf("%s", *separator);
+            }
+        }
+        printf("\n");
+}
