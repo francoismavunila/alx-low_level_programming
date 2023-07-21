f38c1675647f04cb603ccc5f3f92b585d941c6f5
francois <mavunila002@gmail.com>
2023-07-20 19:29:42 -0700

    message

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,58 +1,82 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+void print_int(va_list list);
+void print_float(va_list list);
+void print_char(va_list list);
+void print_str(va_list list);
 
-/**
- * sum_them_all - function that returns the sum of all its parameters
- * @n: number of parameters passed to the function
- *
- * Return: sum of all parameters
- */
+typedef struct printTypeStruct
+{
+char *type;
+void (*printer)(va_list);
+} printTypeStruct;
+
+
+
+void print_int(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+
+void print_float(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+
+void print_char(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+
+
+void print_str(va_list list)
+{
+	char *s = va_arg(list, char *);
+
+	s == NULL ? printf("(nil)") : printf("%s", s);
+
+}
+
+
 
 void print_all(const char * const format, ...)
 {
-    int str_length = 0;
-    char* str;
-    va_list args;
-    va_start(args, format);
-    while(format[str_length] != '\0')
-    {
-        if(format[str_length]=='c'){
-            printf("%c", va_arg(args, int));
-            str_length++;
-            format[str_length] != '\0'? printf(", "): 0;
-        }
-        else if (format[str_length]=='i')
-        {
-            printf("%d", va_arg(args, int));
-            str_length++;
-            format[str_length] != '\0'? printf(", "): 0;
-        }
-        else if (format[str_length]=='f')
-        {
-            printf("%f", va_arg(args, double));
-            str_length++;
-            format[str_length] != '\0'? printf(", "): 0;
-        }
-        else if (format[str_length]=='s')
-        {
-            str = va_arg(args, char *);
-            if (str == NULL)
-            {
-                printf("(nil)");
-            str_length++;
-            format[str_length] != '\0'? printf(", "): 0;
-            }
-            else{
-                printf("%s", str);
-            str_length++;
-            format[str_length] != '\0'? printf(", "): 0;
-            }
+va_list list;
+int i = 0, j = 0;
+char *sep = "";
 
-        }
+printTypeStruct printType[] = {
+	{ "i", print_int },
+	{ "f", print_float },
+	{ "c", print_char },
+	{ "s", print_str },
+	{NULL, NULL}
+};
 
-    }
-    printf("\n");
+
+va_start(list, format);
+
+while (format && format[i])
+{
+	j = 0;
+	while (j < 4)
+	{
+		if (*printType[j].type == format[i])
+		{
+			printf("%s", sep);
+			printType[j].printer(list);
+			sep = ", ";
+			break;
+		}
+		j++;
+	}
+	i++;
 }
-                                                                                                                                   
+
+printf("\n");
+va_end(list);
+}
                                                                                                                                                                                                                                                                 
