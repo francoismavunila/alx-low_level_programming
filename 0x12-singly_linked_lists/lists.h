97f7c4d20a69ea260978b294e1ca6a9d4f7b064b
francois <mavunila002@gmail.com>
2023-07-27 23:49:08 -0700

    message

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,14 +1,7 @@
-#ifndef LIST
-#define LIST
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdlib.h>
 #include <string.h>
-
-/**
- * prints all the elements of a linked list
- * and
- *
- * Return: the number of nodes printed
- */
 
 typedef struct list_s
 {
@@ -18,9 +11,8 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+
 
 #endif
+
+
