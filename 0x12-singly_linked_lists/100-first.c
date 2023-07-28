8373972e486d2bce0ebeb4943f4768a5ed430bb6
francois <mavunila002@gmail.com>
2023-07-28 03:02:36 -0700

    message

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-void first(void) __attribute__ ((constructor));
+void fun(void) __attribute__ ((constructor));
 /**
  * 
  * Return: the number of nodes
  *
  * Return: the number of nodes printed
  */
-void first(void)
+void fun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
