b3ea95725448d0d1b95126cdeb30db0a0d0f56ad
francois <mavunila002@gmail.com>
2023-07-28 10:47:42 -0700

    trying to boost

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * comments here
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int cnt = 0;
+	int x = 0;
+	int y = 0;
+	int z = 0;
+	char *variable;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	while (x < ac)
+	{
+		y = 0;
+		while (av[x][y] != '\0')
+		{
+			cnt++;
+			y++;
+		}
+		x++;
+	}
+	cnt = cnt + ac + 1;
+	variable = malloc(sizeof(char) * cnt);
+	if (variable == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; x < ac; x++)
+	{
+		for (y = 0; av[x][y] != '\0'; y++)
+		{
+			variable[z] = av[x][y];
+			z++;
+		}
+		variable[z] = '\n';
+		z++;
+	}
+	return (variable);
+}
+
