af737675755f5aa2cc72eb75b412adfcbed27142
francois <mavunila002@gmail.com>
2023-07-28 03:01:04 -0700

    message

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
+/**
+ * 
+ * Return: the number of nodes
+ *
+ * Return: the number of nodes printed
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
+
+
