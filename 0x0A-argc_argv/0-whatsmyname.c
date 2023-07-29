90132e7ebf4bafb86bbdc985848f30c15f2bda71
francois <mavunila002@gmail.com>
2023-07-29 00:34:30 -0700

    message

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include "main.h"
 /**
- * pointers and arrays
- * 
+ *pointers and arrays
+ *
  */
 int main(int argc, char *argv[])
 {
-	if (argc>0)
+	if (argc > 0)
 	{
-    printf("%s",argv[0]);
+		printf("%s", argv[0]);
 	}
-    printf("\n");
-    return 0;
+	printf("\n");
+	return (0);
 }
