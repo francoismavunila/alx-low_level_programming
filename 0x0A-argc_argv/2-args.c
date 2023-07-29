5161d50b12eff50441ce2d1eee0f50e775865c2b
francois <mavunila002@gmail.com>
2023-07-29 01:36:42 -0700

    message

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,15 +2,15 @@
 #include "main.h"
 /**
  * pointers and arrays
- * 
+ *
  */
-int main(int argc, char *argv[])
+int main (int argc, char *argv[])
 {
-    int length = 0;
-    while(length<argc)
-    {
-        printf("%s\n",argv[length]);
-	length++;
-    }
+	int length = 0;
+	while (length < argc)
+    	{
+		printf ("%s\n", argv[length]);
+		length++;
+    	}
 	return (0);
 }
