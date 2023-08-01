e191cfcbf2f18c739a37d62022e17f36d65b5a3e
francois <mavunila002@gmail.com>
2023-08-01 01:52:03 -0700

    message

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len- function for that prints the nodes
+ * @h: the pointer to the first node
+ * Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+        size_t count = 0;
+
+        while (h != NULL)
+        {
+                h = h->next;
+                count++;
+        }
+        return (count);
+}
