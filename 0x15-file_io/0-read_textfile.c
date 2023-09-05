6f6d430c8f26849fa8475437ea5373395ed4d804
Francois Mavunila <mavunila002@gmail.com>
2023-09-04 18:32:48 -0700

    commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,33 +2,37 @@
  #include <stddef.h>
  #include "main.h"
  #include <fcntl.h>
+ #include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
   int fd;
   char *buffer;
   ssize_t read_bytes,wr;
-  if(*filename == NULL)
+  if(filename == NULL)
   {
     return(0);
   }
-  fd = open(*filename,O_RDONLY);
+  fd = open(filename,O_RDONLY);
   if(fd == -1)
   {
     return(0);
   }
   else
   {
-    //reading file
     buffer = malloc(letters);
     read_bytes = read(fd,buffer,letters);
-    if(read_bytes == -1 | (read_bytes<letters))
+    if(read_bytes == -1)
     {
         return(0);
     }
     else
     {
         wr = write(STDOUT_FILENO, buffer, read_bytes);
+        if(wr == -1)
+        {
+            return(0);
+        }
     }
     close(fd);
     return(read_bytes);
